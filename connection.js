5ab296a2554143534f52936a4b1a175502481e09
Jan li <bingox29@gmail.com>
2023-02-07 18:17:06 -0500

    change database url

--- connection.js
+++ connection.js
@@ -1,11 +1,25 @@
-const Pool = require("pg").Pool;
+require("dotenv").config();
 
+const { Pool, Client } = require("pg");
+// const DATABASE_URL =
+//   "postgresql://postgres:frankie1220@localhost:5432/hit-the-spot";
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  port: 5432,
-  password: "frankie1220",
-  database: "hit-the-spot",
+  connectionString,
 });
+const client = new Client({
+  connectionString,
+});
+client.connect();
+
+// pool.connect();
+
+// const pool = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   port: 5432,
+//   password: "frankie1220",
+//   database: "hit-the-spot",
+// });
 
 module.exports = pool;
