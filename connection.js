a98f58bc9325481cf6023cb044d007d0b4c559dc
Jan li <bingox29@gmail.com>
2023-02-08 12:49:54 -0500

    change db info

--- connection.js
+++ connection.js
@@ -1,22 +1,30 @@
 require("dotenv").config();
 
-const { Pool, Client } = require("pg");
-const pg = require("pg");
+const Pool = require("pg").Pool;
+const connectionString = process.env.DATABASE_URL;
+
 // const DATABASE_URL =
 //   "postgresql://postgres:frankie1220@localhost:5432/hit-the-spot";
-const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString,
 });
-const client = new Client({
-  connectionString,
-});
-client.connect();
 
-if (process.env.HEROKU_POSTGRESQL_TEAL_URL) {
-  pg.defaults.ssl = { rejectUnauthorized: false };
-}
-pool.connect();
+module.exports = pool;
+
+// const connectionString = process.env.HEROKU_POSTGRESQL_TEAL_URL;
+
+// if (process.env.HEROKU_POSTGRESQL_TEAL_URL) {
+//   const pool = new Pool({
+//     connectionString: process.env.HEROKU_POSTGRESQL_TEAL_URL,
+//     ssl: true,
+//   });
+// } else {
+//   const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   });
+// }
+// module.exports = pool;
+// client.connect();
 
 // const pool = new Pool({
 //   host: "localhost",
@@ -25,8 +33,6 @@ pool.connect();
 //   password: "frankie1220",
 //   database: "hit-the-spot",
 // });
-
-module.exports = pool;
 
 // {
 //   pool,
