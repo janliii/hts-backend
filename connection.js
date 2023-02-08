46d13126227bdd72bde78f38f3caea17efd3b9dd
Jan li <bingox29@gmail.com>
2023-02-08 12:53:08 -0500

    ssl true

--- connection.js
+++ connection.js
@@ -7,6 +7,7 @@ const connectionString = process.env.DATABASE_URL;
 //   "postgresql://postgres:frankie1220@localhost:5432/hit-the-spot";
 const pool = new Pool({
   connectionString,
+  ssl: true,
 });
 
 module.exports = pool;
