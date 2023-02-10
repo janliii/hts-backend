4908d18b0d3968284f9f1a534bcc5fa1cbf96f27
Jan li <bingox29@gmail.com>
2023-02-10 01:26:23 -0500

    add weather query

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const pkg = require("node-wit");
 const { Wit } = pkg;
+const axios = require("axios");
 
 const accessToken = process.env.WIT_AI_TOKEN;
 
@@ -64,12 +65,36 @@ const handleResponse = (response) => {
 //         return console.log("Please don't forget good manners, say hello first!");
 //         // return handleGibberish();
 // }
+const searchWikipedia = async (searchQuery) => {
+  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+  const response = await fetch(endpoint);
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  const json = await response.json();
+  console.log(json);
+  return json;
+};
 
+const getWeather = async (lat, lon) => {
+  const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API}`;
+  try {
+    const response = await axios.get(endpoint);
+    const currentTemp = Math.floor(
+      (Number(await response.data.main.temp) - 273.15) * 1.8 + 32
+    );
+    const weatherDes = await response.data.weather[0].description;
+    const city = await response.data.name;
+    // return response;
+    return `It is ${weatherDes} in ${city}. The current temperature is ${currentTemp} degrees`;
+  } catch (err) {
+    console.error("error in findweather!", err);
+  }
+};
 // HANDLE different functions
 const handleHello = () => {
   return "Hello, can I help you?";
 };
-// HANDLE GIBBERISH
 const handleGibberish = () => {
   return "Sorry, I don't understand. Can you say that again?";
 };
@@ -77,7 +102,10 @@ const handleLocation = (r) => {
   const location = r.entities["wit$location:location"][0].body;
   const timezone =
     r.entities["wit$location:location"][0].resolved["values"][0]["timezone"];
-  return `${location} is in ${timezone} timezone`;
+  // const wikiRes = searchWikipedia(location).then((json) => {
+  //   json;
+  // });
+  return `${location} is in ${timezone} timezone. `;
 };
 
 const handleTime = (r) => {
@@ -89,7 +117,21 @@ const handleDate = (r) => {
   return `Today is ${time.slice(0, 10)} `;
 };
 const handleWeather = (r) => {
-  return `It is raining now `;
+  if (r.entities["wit$location:location"]) {
+    const location = r.entities["wit$location:location"][0].body;
+    const lat =
+      r.entities["wit$location:location"][0].resolved["values"][0]["coords"][
+        "lat"
+      ];
+    const long =
+      r.entities["wit$location:location"][0].resolved["values"][0]["coords"][
+        "long"
+      ];
+    return getWeather(lat, long);
+    console.log(getWeather(lat, long));
+  } else {
+    return `please specify your city. For example,say "what's the weather in my_city?" `;
+  }
 };
 
 module.exports = {
