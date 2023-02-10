116e6df98df8ed1182369bda4b939076ee3ca84e
Jan li <bingox29@gmail.com>
2023-02-10 15:34:50 -0500

    add table

--- connection.js
+++ connection.js
@@ -7,9 +7,10 @@ const connectionString = process.env.DATABASE_URL;
 //   "postgresql://postgres:frankie1220@localhost:5432/hit-the-spot";
 const pool = new Pool({
   connectionString,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    // require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
